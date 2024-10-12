5769fba32d5490536615660955dcc50085ac8dd4
Joseph <philippdeutsch@icloud.com>
2024-10-11 21:27:02 -0500

    gpt-4o-mini

--- langchain_helper.py
+++ langchain_helper.py
@@ -28,7 +28,7 @@ def get_retriever_from_database(_db):
 
 @st.cache_resource
 def get_language_model():
-    llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
+    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
     return llm
 
 @st.cache_resource
